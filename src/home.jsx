0962e64578c50dcacb62155dfc995100f199b756
SunSpes <romain.combier13@gmail.com>
2026-07-16 18:17:23 +0200

    icon + seo

--- src/home.jsx
+++ src/home.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import bg from './assets/bgnotext.mp4'
+import bg from './assets/bg/bgnotext.mp4'
 import logo from './assets/LOGO.png'
 import { Helmet } from 'react-helmet-async'
 
@@ -18,8 +18,8 @@ function App() {
 
 {/* Contenu principal */}
       <div className="bg-stone-900 overflow-hidden  md:bg-transparent flex items-center justify-center h-screen w-screen">
-        <video autoPlay loop muted playsInline className="absolute inset-0 w-full h-full object-cover z-0">
-          <source src={bg}/>
+        <video autoPlay loop muted playsInline preload='auto' className="absolute inset-0 w-full h-full object-cover z-0">
+          <source src={bg} type="video/mp4" />
         </video>
         <img src={logo} alt="Logo" className="absolute z-10 w-100 mb-44" />
 
--- src/montage.jsx
+++ src/montage.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Helmet } from 'react-helmet-async'
 import bg from './assets/projet/test.avif'
+import fondmontage from './assets/bg/fondmontage.mp4'
 import alucard from './assets/projet/concept.jpg'
 import machli from './assets/projet/machli.jpg'
 import requin from './assets/projet/requin.jpg'
@@ -67,6 +68,7 @@ function Montage() {
         <meta name="description" content="Portfolio montage vidéo de Sunspes : storytelling, gaming, projets créatifs. Motion designer basé en France." />
         <link rel="canonical" href="https://sunspes.fr/montage/" />
       </Helmet>
+
       {/* Contenu principal */}
       {projects.map((project) => (
         <div key={project.id} className="md:hover:scale-105 md:hover:drop-shadow-lg transition-all duration-300 ease-in-out">
