013d8677d28baf43e6e1d606efbee5b907641dcc
SunSpes <romain.combier13@gmail.com>
2026-07-17 12:03:18 +0200

    sitemap SEO google

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://sunspes.fr', // remplace par ton vrai domaine
+      dynamicRoutes: [
+        '/montage',
+        '/motion',
+        // '/contact', // à décommenter quand tu l'ajoutes
+      ],
+    }),
+  ],
 })
